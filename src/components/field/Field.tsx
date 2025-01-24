fa4881514dbcd64ddefc0da4e4c6395217af3601
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-24 22:25:23 +0000

    Jan 24, 2025, 11:25 PM

--- src/components/field/Field.tsx
+++ src/components/field/Field.tsx
@@ -22,8 +22,8 @@ const Sheet = ({ className, ...restProps }: React.ComponentProps<'div'>) => {
     <div
       {...restProps}
       className={twMerge(
-        'ring-1 ring-gray-200 rounded-lg overflow-clip w-full flex hover:ring-gray-400',
-        meta.touched && 'ring-blue-500 ring-2',
+        'ring-1 ring-gray-200 rounded-lg overflow-clip w-full flex hover:ring-gray-400 transition-all duration-200 ease-in-out',
+        meta.touched && 'ring-blue-500 ring-2 hover:ring-blue-500',
         className
       )}
     />
