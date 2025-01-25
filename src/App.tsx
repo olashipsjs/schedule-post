4dbff27249a46a6c1b85182eedb631e6354db675
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-25 07:30:48 +0000

    ✨ Add GSAP animations and schedule button functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,28 @@ import Textarea from './components/textarea/Textarea';
 import Field from './components/field/Field';
 import { twMerge } from 'tailwind-merge';
 import Button from './components/button/Button';
+import { useGSAP } from '@gsap/react';
+import React from 'react';
+import gsap from 'gsap';
 
 const App = () => {
+  const [isHidden, setIsHidden] = React.useState(true);
+
+  useGSAP(() => {
+    gsap.to('#action-btn', {
+      duration: 0.2,
+      ease: 'power3.inOut',
+      width: isHidden ? 'fit-content' : '100%',
+    });
+    gsap.to('#notification', {
+      duration: 0.2,
+      ease: 'power3.inOut',
+      zIndex: isHidden ? -1 : 1,
+      marginTop: isHidden ? -32 : 0,
+      opacity: isHidden ? 0 : 1,
+    });
+  }, [isHidden]);
+
   return (
     <main className='flex flex-col items-center justify-center bg-gray-50 min-h-screen'>
       <section className='w-full'>
@@ -22,15 +42,27 @@ const App = () => {
               >
                 <Field.Sheet
                   className={twMerge(
-                    'bg-white rounded-3xl overflow-clip ring-1 ring-gray-200 flex-col'
+                    'bg-white rounded-3xl overflow-clip ring-1 ring-gray-200 flex-col gap-3'
                   )}
                 >
                   <Textarea placeholder={"What's up?"} />
                   <div className='flex gap-2 items-center justify-end p-2'>
-                    <Button>Post</Button>
+                    {isHidden ? (
+                      <Button
+                        id='switch-btn'
+                        onClick={() => setIsHidden(!isHidden)}
+                        className={'bg-gray-200 text-gray-900'}
+                      >
+                        Schedule
+                      </Button>
+                    ) : null}
+                    <Button id='action-btn'>Post</Button>
                   </div>
                 </Field.Sheet>
-                <div className='p-1.5'>
+                <div
+                  id='notification'
+                  className='p-1.5'
+                >
                   <p className='text-center text-gray-500 text-sm font-medium'>
                     Will be posted on 25 Feb, 9:30AM
                   </p>
