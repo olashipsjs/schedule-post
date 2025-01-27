8c6183c84a28cd1b0cf03fd58aedf045eedacee0
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-27 15:15:05 +0000

    Jan 27, 2025, 4:15 PM

--- src/overlay/Overlay.tsx
+++ src/overlay/Overlay.tsx
@@ -1,5 +1,73 @@
-const Overlay = () => {
-  return <div>Overlay</div>;
+import React from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import useOverlay from './useOverlay';
+import OverlayProvider from './Provider';
+import Button from '../components/button/Button';
+
+const Compound = ({
+  open,
+  children,
+  ...restProps
+}: Omit<
+  React.ComponentProps<'div'>,
+  keyof React.ComponentProps<typeof OverlayProvider>
+> &
+  React.ComponentProps<typeof OverlayProvider>) => {
+  return (
+    <div {...restProps}>
+      <OverlayProvider
+        open={open}
+        children={children}
+      />
+    </div>
+  );
 };
 
-export default Overlay;
+const Trigger = ({
+  onClick,
+  ...restProps
+}: React.ComponentProps<typeof Button>) => {
+  const { open } = useOverlay();
+
+  const handleClick = (ev: React.MouseEvent<HTMLButtonElement>) => {
+    open();
+    onClick && onClick(ev);
+  };
+
+  return (
+    <Button
+      {...restProps}
+      onClick={handleClick}
+    />
+  );
+};
+
+const Content = ({ ...restProps }: React.ComponentProps<'div'>) => {
+  const { isOpen } = useOverlay();
+  const ref = React.useRef<HTMLDivElement>(null!);
+
+  useGSAP(() => {
+    const current = ref.current;
+    gsap.to(current, {
+      ease: 'power1.inOut',
+      duration: 0.2,
+      opacity: isOpen ? 1 : 0,
+    });
+  }, [isOpen]);
+
+  return (
+    <div
+      ref={ref}
+      {...restProps}
+    />
+  );
+};
+
+const Overlay = Compound as typeof Compound & {
+  Trigger: typeof Trigger;
+  Content: typeof Content;
+};
+
+Overlay.Trigger = Trigger;
+Overlay.Content = Content;
--- src/overlay/useOverlay.tsx
+++ src/overlay/useOverlay.tsx
@@ -1,3 +1,16 @@
-const useOverlay = () => {};
+import React from 'react';
+import { OverlayContext } from './Provider';
+
+const useOverlay = () => {
+  const context = React.useContext(OverlayContext);
+
+  if (!context) {
+    throw new Error(
+      'useOverlay hook cannot be defined outside the OverlayProvider component'
+    );
+  }
+
+  return context;
+};
 
 export default useOverlay;
