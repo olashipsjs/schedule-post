c0f90cd7dcfa4d372aea5d64e2157dfdda652ac5
olashipsjs <olanrewajuoladejo08@gmail.com>
2025-01-30 18:56:21 +0000

    🕒 Update hours array to start from 1 and change Select.Content overflow property to clip

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import CalenderIcon from './components/icons/CalenderIcon';
 import { useGSAP } from '@gsap/react';
 import Select from './components/select/Select';
 
-const hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const minutes = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
   22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
@@ -122,7 +122,7 @@ const App = () => {
                                   }}
                                 </Select.Value>
                               </Select.Trigger>
-                              <Select.Content className='bg-white rounded-2xl h-64 overflow-y-scroll p-4 flex-col flex justify-start gap-y-4'>
+                              <Select.Content className='bg-white rounded-2xl h-64 overflow-y-clip p-4 flex-col flex justify-start gap-y-4'>
                                 <h6 className='px-3 font-semibold text-center'>
                                   Time
                                 </h6>
